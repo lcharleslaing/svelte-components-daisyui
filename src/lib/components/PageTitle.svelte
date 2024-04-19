6e4e4642707c22208110260bf13b049194975f0f
Lee Charles Laing <71520336+lcharleslaing@users.noreply.github.com>
2024-04-19 13:15:32 -0400

    chore: Update PageTitle component with new margin-top value

--- src/lib/components/PageTitle.svelte
+++ src/lib/components/PageTitle.svelte
@@ -25,9 +25,10 @@
 	});
 </script>
 
-<div class="mt-20 text-center">
-	<h1 class="text-4xl font-extrabold">{pageTitle}</h1>
-	<!-- <TgyLogo height="48" /> -->
+<div class="mt-6 flex justify-center">
+	<div class="bg-base-300 px-6 py-2 shadow-sm">
+		<h1 class="pb-2 text-4xl font-extrabold">{pageTitle}</h1>
+	</div>
 </div>
 
 {#if devmode}
